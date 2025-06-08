aa54c50bb3d625f0c14240995daef859fcf1fabb
saiprathap-projects <tosaiprathapb@gmail.com>
2025-06-08 22:11:23 +0530

    first commit

---
+++ src/test/java/Weather_application/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package Weather_application.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
