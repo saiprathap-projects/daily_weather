5d191fd9db90c9095f08c50390557ff11ad23cdf
Saiprathap <tosaiprathapb@gmail.com>
2025-06-24 00:12:32 +0530

    Update main.tf

--- terraform/ECR/main.tf
+++ terraform/ECR/main.tf
@@ -1,9 +1,6 @@
 provider "aws" {
   region = var.region
 }
-resource "aws_ecr_repository" "maven_artifacts" {
-  name = var.repository_name
-}
 resource "aws_ecr_repository" "tomcat" {
   name = var.repository_name_1
 }
